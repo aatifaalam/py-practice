871a6ad415c6894bfd69787c32bec23d16b94fea
Aatif <alamatif615@gmail.com>
2026-02-22 17:32:18 +0530

    test1

--- arr.py
+++ arr.py
@@ -5,4 +5,15 @@ arr = [
     [16, 17, 18, 19, 20],
     [21, 22, 23, 24, 25]
     ]
-print(arr[4][2])
+
+arr2 = [
+    [55, 56, 57],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+arr3 = arr + arr2
+# print(arr3)
+print("Value of first arr:", arr[1][2])
+print("Value of second arr:", arr2[0][0])
+print("Addition of both values are: ", arr[1][2] + arr2[0][0])
