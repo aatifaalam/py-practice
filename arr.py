00c033d102dedf3e02fc43800b519a4468c4356c
TaleevAalam <alammdtalib@gmail.com>
2026-02-23 14:12:40 +0530

    Update arr.py

--- arr.py
+++ arr.py
@@ -2,11 +2,11 @@ arr = [
     [1, 2, 3, 4, 5],
     [6, 7, 8, 9, 10],
     ]
-
+total=0
 for i in range(1, len(arr)):
     for j in range (0, len(arr[i])):
-        b = arr[0][j] + arr[i][j]
-        print(b)
+        total=total+1
 
         # print(arr[0][0] + arr[0][1] + arr[0][2] + arr[0][3] + arr[0][4])
         # print(arr[1][0] + arr[1][1] + arr[1][2] + arr[1][3] + arr[1][4])
+print(total)
