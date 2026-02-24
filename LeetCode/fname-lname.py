f2474d1dd7f6cde767e5a61022202d9274db35f2
TaleevAalam <alammdtalib@gmail.com>
2026-02-24 13:24:10 +0530

    Update fname-lname.py

--- LeetCode/fname-lname.py
+++ LeetCode/fname-lname.py
@@ -2,6 +2,9 @@ fname = ["Aatif", "Taleev", "Wasif"]
 lname = ["Aalam", "Aalam2", "Aalam3"]
 
 finalName = []
+#this parameter 0 is unncessary in range method since start=0 already by default
 for i in range(0, len(fname)):
     finalName.append(fname[i] + " " + lname[i])
     print(finalName[i])
+
+#create a seperate loop on finalName to print the full names
