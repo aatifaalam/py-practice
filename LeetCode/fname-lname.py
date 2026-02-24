c9254694c4381011c796e6f5e4bf4c2f8b386d8c
Aatif <alamatif615@gmail.com>
2026-02-24 14:01:49 +0530

    .

--- LeetCode/fname-lname.py
+++ LeetCode/fname-lname.py
@@ -1,10 +1,10 @@
 fname = ["Aatif", "Taleev", "Wasif"]
 lname = ["Aalam", "Aalam2", "Aalam3"]
 
+lname.reverse()
 finalName = []
 for i in range(len(fname)):
     finalName.append(fname[i] + " " + lname[i])
-    # print(finalName[i])
 
 for i in range(len(finalName)):
     print(finalName[i])
