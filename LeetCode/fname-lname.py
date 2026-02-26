b042238852cd4db307334a1cb66ec111e5439ab5
Aatif <alamatif615@gmail.com>
2026-02-26 14:49:00 +0530

    .

--- LeetCode/fname-lname.py
+++ LeetCode/fname-lname.py
@@ -1,16 +1,22 @@
+class Name:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def getFinalName(self):
+        finalName = []
+        j = len(self.lname)-1
+        for i in range(len(self.fname)):
+            finalName.append(self.fname[i] + " " + self.lname[j])
+            j -= 1
+        return finalName
+
 fname = ["Aatif", "Taleev", "Wasif"]
 lname = ["Aalam", "Aalam2", "Aalam3"]
 
-j = len(lname)-1
+n = Name(fname, lname)
 
-def getFinalName():
-    finalName = []
-    j = len(lname)-1
-    for i in range(len(fname)):
-        finalName.append(fname[i] + " " + lname[j])
-        j -= 1
-    return finalName
-finalName = getFinalName()
+finalName = n.getFinalName()
 
-for i in range(len(finalName)):
-    print(finalName[i])
+for i in finalName:
+    print(i)
--- LeetCode/met.py
+++ LeetCode/met.py
@@ -1,13 +1,15 @@
-class Rectangle:
+class ATM:
+     def __init__(self, name, balance):
+              self.name = name
+              self.balance = balance
+     def deposit(self, amount):
+           self.balance = self.balance + amount
+     def withdraw(self, amount):
+           self.balance = self.balance - amount
+     def show_balance(self):
+           print(self.name, "Balance: " , self.balance)
 
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        print(self.length * self.width)
-
-
-r = Rectangle(10, 5)
-r.area()
-# print(r.area())
+a = ATM("Aatif",1000)
+a.deposit(500)
+a.withdraw(200)
+a.show_balance()
