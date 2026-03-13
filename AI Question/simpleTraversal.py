a947304ea2b8b4b30afe6b2d2e738914ea503756
Aatif <alamatif615@gmail.com>
2026-03-13 23:30:54 +0530

    .

--- AI Question/simpleTraversal.py
+++ AI Question/simpleTraversal.py
@@ -1,5 +1,7 @@
-nums = [4, 7, 2, 9, 5]
+# nums = [4, 7, 2, 9, 5]
 
-for i in range(len(nums)):
-    print(i)
-    print(nums[i])
+# for i in range(len(nums)):
+#     print(i)
+#     print(nums[i])
+
+print("2" + "k" * 2)
