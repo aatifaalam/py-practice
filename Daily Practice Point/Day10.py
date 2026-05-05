666fce637db9b138667c665a68bec54b1bd20c6c
Aatif Aalam <alamatif615@gmail.com>
2026-05-06 02:12:33 +0530

    Day 1 of learning fastapi

--- Daily Practice Point/Day10.py
+++ Daily Practice Point/Day10.py
@@ -1,1 +1,8 @@
-from fastapi import FastAPI
+users = {}
+n = int(input("Enter numbers of users: "))
+for i in range(n):
+    name = input("Enter name: ")
+    id = int(input("Enter id: "))
+    users[name] = id
+
+print(users)
--- Python-Learning/api-1.py
+++ Python-Learning/api-1.py
@@ -5,6 +5,38 @@ app = FastAPI()
 def home():
     return {"meassage": "Hello, Aatif"}
 
+# users = {}
+# number_of_users = int(input("Enter numbers of users: "))
+# @app.get("/add-users")
+# def add_users(users):
+#     for i in range(number_of_users):
+#         name = input("Enter name: ")
+#         id = int(input("Enter id: "))
+#         users[name] = id 
+#         return {"message": "User Added!"}
+
+users = {}
+@app.get("/add_users")
+def add_users(name: str, age: int):
+    users[name] = age
+    return {"message": "User Added"}
+      
+
+@app.get("/get_user")
+def get_users(users):
+    return users
+
+@app.get("/user/{name}")
+def get_user(name: str):
+    if name in users:
+        return {name: users[name]}
+    return {"error": "User not found"}
+
+@app.get("/update_user/{name}")
+def update_user(name: str, age: int):
+    users.update({name:age})
+    return {"meassage": "User Updated"}, users
+
 @app.get("/home")
 def hello():
     return {"message": "Hello, Aatif! Welcome to FastAPI"}
