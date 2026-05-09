d4a0e252a6cedd2b8064a4bd0d6321362342143d
Aatif Aalam <alamatif615@gmail.com>
2026-05-09 17:59:52 +0530

    Practice

--- LeetCode Problems✅/Recall_Point.py
+++ LeetCode Problems✅/Recall_Point.py
@@ -1,18 +1,23 @@
-s = "anagram"
-t = "nagaram"
-freq = {} 
-def valid_anagram(s, t):
-    if len(s) != len(t):
-        return False
-    for i in s:
-        if i in s:
-            freq[i] = freq.get(i, 0) + 1
-    for i in t:
-        if i not in freq:
-            return False
-        freq[i] = freq[i] - 1
-        if freq[i] < 0:
-            return False
-    return True    
+# s = "anagram"
+# t = "nagaram"
+# freq = {} 
+# def valid_anagram(s, t):
+#     if len(s) != len(t):
+#         return False
+#     for i in s:
+#         if i in s:
+#             freq[i] = freq.get(i, 0) + 1
+#     for i in t:
+#         if i not in freq:
+#             return False
+#         freq[i] = freq[i] - 1
+#         if freq[i] < 0:
+#             return False
+#     return True    
 
-print(valid_anagram(s, t))
+# print(valid_anagram(s, t))
+
+n = 5
+for i in range(n):
+    a = 1 * n
+    print(a * "*")
